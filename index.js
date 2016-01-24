4fa07a0cff79733437a151a911e6f796f5c66e8d
Parth Desai <parthdesai95@gmail.com>
2016-01-24 18:53:08 -0500

    nicknames added

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var numberOnline = 0;
+var users = {};
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', function(socket){
   console.log("a user connected");
-  io.emit('welcome message');
+  numberOnline += 1;
+  socket.on('nickname', function(msg){
+      users[socket.id] = msg;
+
+      io.emit('welcome message', msg);
+      console.log('new user: ' + msg + ' number online: ' + numberOnline.toString());
+      console.log(users);
+  });
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('messages: '+msg);
+    var user = users[socket.id] + ": ";
+    io.emit('chat message', user+msg);
+    console.log(user + ': '+msg);
   });
   socket.on('disconnect', function(){
     console.log('user disconnect');
+    numberOnline -= 1;
   });
 });
 http.listen(3000, function() {
